aaddbc49965f4b7e420a2151d3b5040ee44a8d67
ibox45 <ibrahimakdeniz@gmail.com>
2022-07-02 13:45:13 +0300

    JDBC

---
+++ src/test/java/runners/TestRunner.java
@@ -0,0 +1,37 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin={"html:target/cucumber-reports.html",
+                "json:target/json-reports/cucumber.json",
+                "junit:target/xml-report/cucumber.xml"
+        },
+        features = "src/test/resources/features",
+        glue="stepDefinitions",
+        tags ="@db",
+        dryRun = false
+)
+
+public class TestRunner {
+    /*
+    Bir framework'de bir tek Runner class'i yeterli olabilir
+    Runner Classında class body'sinde hiç bir şey olmaz
+    Runner classımızı onemli yapan 2 adet annotaion vardir
+    @RunWith(Cucumber.class) notasyonu Runner classında calısma ozellıgı katar
+    Bu notasyon oldugu icin Cucumber framework'umuzde JUnit kullanmayi tercih ediyoruz.
+
+    features : Runner dosyasının feature  dosyalarını nereden bulacagını tarif eder
+    glue: step definitions dosyalarını nerede bulacagımızı gosterir
+    tags: o an hangi tag'i çalıstırmak istiyorsak onu belli eder
+
+    dryRun : iki secenek var
+    dryRun=true; yazdıgımızda Testimizi calıstırmadan sadece eksik adımları bize verir
+    dryRun=false; yazdıgımızda testlerimizi çalistirir.
+
+
+     */
+}
---
+++ src/test/java/stepDefinitions/HMC_db_Stepdefinitions.java
@@ -0,0 +1,58 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Given;
+
+import java.sql.*;
+
+
+public class HMC_db_Stepdefinitions {
+
+    String url="jdbc:sqlserver://184.168.194.58:1433;databaseName=hotelmycamp ;"+
+            "user=techproed;password=P2s@rt65";
+    String username="techproed";
+    String password="P2s@rt65";
+
+    Connection connection; // DATAbase 'e bağlanmamız sağlar
+    Statement statement; // query calıştırmamız sağlıyor
+    ResultSet resultSet; // query sonucunda dönen sonucu sto etmemiziz sağlıyor
+    @Given("kullanici HMC veri tabanina baglanir")
+    public void kullanici_hmc_veri_tabanina_baglanir() throws SQLException {
+        connection= DriverManager.getConnection(url,username,password);
+        statement=connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+    }
+    @Given("kullanici {string} tablosundaki {string} verilerini alir")
+    public void kullanici_tablosundaki_verilerini_alir(String table, String field) throws SQLException {
+        String query ="SELECT " +field+" FROM "+ table;
+        resultSet=statement.executeQuery(query);
+    }
+    @Given("kullanici {string} sutunundaki verileri okur")
+    public void kullanici_sutunundaki_verileri_okur(String field) throws SQLException {
+
+        //resultset iterator gibi calisir
+        resultSet.first();
+        System.out.println((resultSet.getString("price")));  //225.0000
+        resultSet.next();  //itarator'a benzer şekilde calısır.
+                            // next()'u imleci bir sonraki degerin yanına goturur
+                           // bize true veya false doner.
+        System.out.println((resultSet.getString("price")));  //4000.0000
+
+        System.out.println(resultSet.next());  //true
+        System.out.println((resultSet.getString("price")));   //445.0000
+        //next() kullanılırken cok dikkatli olmalıyız.
+        //cunku nerede olursa olsun imleci bir sonraki elemente gecirecektir.
+
+        System.out.println("==========Liste=========");
+        resultSet.absolute(0);
+       // int sira=1;
+        while(resultSet.next()){
+            System.out.println(resultSet.getRow()+". kayit: "+(resultSet.getString("price")));
+           // sira++;  sira. kayıt diyordum getRow() 'u ogrenince bu şekilde duzenledım.
+        }
+
+        //tabloda kaç satir oldugunu nasil buluruz
+        resultSet.last();
+        System.out.println(resultSet.getRow());
+
+
+    }
+}
